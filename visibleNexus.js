e00cc63a75f77290c9184c370a3c2c83702b3130
Simon Bates <simon@bitstructures.com>
2017-03-06 14:15:22 -0500

    GPII-2305: Make the nexusComponentRoot visible

--- visibleNexus.js
+++ visibleNexus.js
@@ -16,6 +16,10 @@ var kettle = require("kettle");
 
 require("./index.js");
 
+fluid.construct("globalNexusComponentRoot", {
+    type: "gpii.nexus.nexusComponentRoot"
+});
+
 kettle.config.loadConfig({
     configName: kettle.config.getConfigName("fluid.visible.nexus.config"),
     configPath: kettle.config.getConfigPath("%fluid-authoring/src/server/configs")
